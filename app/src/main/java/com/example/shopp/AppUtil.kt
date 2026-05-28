41932b0d00b4eb4d161199c3e57adb8c0c78b91c
Vaibhav Pandey <pandeyvaibhavdev7505@gmail.com>
2026-05-28 17:14:51 +0530

    Cart Page

--- app/src/main/java/com/example/shopp/AppUtil.kt
+++ app/src/main/java/com/example/shopp/AppUtil.kt
@@ -3,13 +3,14 @@ package com.example.shopp
 import android.content.Context
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 object AppUtil {
 
     fun showToast(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 
     fun addItemToCart(productId: String, context: Context) {
@@ -45,5 +46,45 @@ object AppUtil {
             }
 
         }
+    }
+
+    fun removeFromCart(productId: String, context: Context,removeAll : Boolean = false) {
+        val currentUserId = FirebaseAuth.getInstance().currentUser?.uid!!
+        val userDoc =
+            Firebase.firestore            //Taking document reference because we want to update it
+                .collection("users")
+                .document(currentUserId)
+
+        userDoc.get().addOnCompleteListener {
+            if (it.isSuccessful) {
+                // Fetch the existing map of cart items or initialize an empty one
+                val currentCart = it.result.get("cartItems") as? Map<String, Long> ?: emptyMap()
+
+                // Read current product quantity or fallback to 0
+                val currentQuantity = currentCart[productId] ?: 0L
+                val updatedQuantity = currentQuantity - 1
+
+
+                // Create map variable targeting structural nested values updates
+                val updatedCart =
+                    if (updatedQuantity <=0 || removeAll)
+                        mapOf("cartItems.$productId" to FieldValue.delete())
+                    else
+                        mapOf("cartItems.$productId" to updatedQuantity)
+
+                // Update the document in Firestore
+                userDoc.update(updatedCart)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showToast(context, "Item removed from the cart")
+                        } else {
+                            showToast(context, "Failed removing item from the cart")
+                        }
+                    }
+            }
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/shopp/pages/CartPage.kt
+++ app/src/main/java/com/example/shopp/pages/CartPage.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,20 +27,22 @@ fun CartPage(modifier: Modifier = Modifier) {
 
     var userModel = remember { mutableStateOf(UserModel()) }
 
-    LaunchedEffect(Unit) {
+    DisposableEffect(Unit)   {
         val currentUserId = FirebaseAuth.getInstance().currentUser?.uid!!
-        Firebase.firestore
+        var listener = Firebase.firestore
             .collection("users")
             .document(currentUserId)
-            .get()
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    val result = it.result?.toObject(UserModel::class.java)
+            .addSnapshotListener {it,_ ->      //to see changes frequently
+                if (it!=null) {
+                    val result = it.toObject(UserModel::class.java)
                     if (result != null) {
                         userModel.value = result
                     }
                 }
             }
+        onDispose {
+            listener.remove()
+        }
     }
 
     Column(
@@ -55,7 +58,7 @@ fun CartPage(modifier: Modifier = Modifier) {
         )
 
         LazyColumn {
-            items(userModel.value.cartItems.toList()) {(productId,qty) ->
+            items(userModel.value.cartItems.toList(),key = {it.first}) {(productId,qty) ->
                 CartItemView(productId = productId, qty = qty)
             }
         }
